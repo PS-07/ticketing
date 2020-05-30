fa37027b74c206661adb7eea76cb2ad949e051fb
PS-07 <singh.puneet1997@gmail.com>
2020-05-30 21:20:50 +0530

    added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+    console.log('Starting up...');
+
     // check if the secret key is defined
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
